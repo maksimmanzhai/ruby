af28c645c52c0e2289f144f3da2ba6b00b5b6443
maksimmanzhai <bender23max@gmail.com>
2017-12-17 14:54:17 +0300

    Defination functioon greating_area

--- project_1/def.rb
+++ project_1/def.rb
@@ -1,27 +1,22 @@
-def area
+@arr = []
 
+def greating_area x, y
+	puts "======================="
+	for i in (0...y) do
+		for j in (0...x) do
+			@arr[i] = []
+			@arr[i] = (0...x).to_a 
+		end
+	end
 end
 
-def dots
-
+def start_game
+	puts "Hello. Choose your size of area"
+	print "Enter X: "
+	x = gets. chomp.to_i
+	print "Enter Y: "
+	y = gets. chomp.to_i
+	greating_area x, y
 end
 
-def player
-
-end
-
-def keys
-
-end
-
-def links
-
-end
-
-def fields
-
-end
-
-loop do
-	
-end
+start_game
